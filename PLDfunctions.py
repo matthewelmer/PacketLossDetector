d2f73b8548c4453044027a4653b23a7f91547a4b
Matthew D Elmer <matthewelmer@tamu.edu>
2021-08-15 09:37:48 -0500

    tidying up

--- PLDfunctions.py
+++ PLDfunctions.py
@@ -9,18 +9,30 @@ def getInput(inputList):
     server = ''
     if len(inputList) > 1 and inputList[1]:
         server = inputList[1]
+        if server.lower() == 'default':
+            server = '8.8.8.8'  # Google primary DNS
     else:
         server = '8.8.8.8'  # Google primary DNS
 
     testDuration = 0
     if len(inputList) > 2 and inputList[2]:
-        testDuration = int(inputList[2])  # seconds
+        try:
+            testDuration = int(inputList[2])  # seconds
+        except ValueError:
+            pass
+        if inputList[2] == 'default':
+            testDuration = 60  # seconds
     else:
         testDuration = 60  # seconds
 
     packetsPerSecond = 0
     if len(inputList) > 3 and inputList[3]:
-        packetsPerSecond = int(inputList[3])
+        try:
+            packetsPerSecond = int(inputList[3])
+        except ValueError:
+            pass
+        if inputList[3] == 'default':
+            packetsPerSecond = 60
     else:
         packetsPerSecond = 60
 
